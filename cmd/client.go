4832f3aab0b7f677d9e1b2b0c502a6d8bcdcb814
世界 <i@sekai.icu>
2021-12-13 00:09:17 +0800

    Remove unused config

--- cmd/client.go
+++ cmd/client.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"io/ioutil"
 	"net"
-	"net/http"
-	"strings"
 	"time"
 
 	"github.com/lucas-clemente/quic-go"
@@ -16,13 +14,9 @@ import (
 	"github.com/tobyxdd/hysteria/pkg/acl"
 	hyCongestion "github.com/tobyxdd/hysteria/pkg/congestion"
 	"github.com/tobyxdd/hysteria/pkg/core"
-	hyHTTP "github.com/tobyxdd/hysteria/pkg/http"
 	"github.com/tobyxdd/hysteria/pkg/obfs"
-	"github.com/tobyxdd/hysteria/pkg/relay"
 	"github.com/tobyxdd/hysteria/pkg/socks5"
-	"github.com/tobyxdd/hysteria/pkg/tproxy"
 	"github.com/tobyxdd/hysteria/pkg/transport"
-	"github.com/tobyxdd/hysteria/pkg/tun"
 )
 
 func client(config *clientConfig) {
@@ -85,18 +79,6 @@ func client(config *clientConfig) {
 	if len(config.Obfs) > 0 {
 		obfuscator = obfs.NewXPlusObfuscator([]byte(config.Obfs))
 	}
-	// ACL
-	var aclEngine *acl.Engine
-	if len(config.ACL) > 0 {
-		var err error
-		aclEngine, err = acl.LoadFromFile(config.ACL, transport.DefaultTransport)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error": err,
-				"file":  config.ACL,
-			}).Fatal("Failed to parse ACL")
-		}
-	}
 	// Client
 	client, err := core.NewClient(config.Server, auth, tlsConfig, quicConfig, transport.DefaultTransport,
 		uint64(config.UpMbps)*mbpsToBps, uint64(config.DownMbps)*mbpsToBps,
@@ -120,7 +102,7 @@ func client(config *clientConfig) {
 				}
 			}
 			socks5server, err := socks5.NewServer(client, transport.DefaultTransport, config.SOCKS5.Listen, authFunc,
-				time.Duration(config.SOCKS5.Timeout)*time.Second, aclEngine, config.SOCKS5.DisableUDP,
+				time.Duration(config.SOCKS5.Timeout)*time.Second, nil, config.SOCKS5.DisableUDP,
 				func(addr net.Addr, reqAddr string, action acl.Action, arg string) {
 					logrus.WithFields(logrus.Fields{
 						"action": actionToString(action, arg),
@@ -164,225 +146,6 @@ func client(config *clientConfig) {
 			}
 			logrus.WithField("addr", config.SOCKS5.Listen).Info("SOCKS5 server up and running")
 			errChan <- socks5server.ListenAndServe()
-		}()
-	}
-
-	if len(config.HTTP.Listen) > 0 {
-		go func() {
-			var authFunc func(user, password string) bool
-			if config.HTTP.User != "" && config.HTTP.Password != "" {
-				authFunc = func(user, password string) bool {
-					return config.HTTP.User == user && config.HTTP.Password == password
-				}
-			}
-			proxy, err := hyHTTP.NewProxyHTTPServer(client, transport.DefaultTransport,
-				time.Duration(config.HTTP.Timeout)*time.Second, aclEngine,
-				func(reqAddr string, action acl.Action, arg string) {
-					logrus.WithFields(logrus.Fields{
-						"action": actionToString(action, arg),
-						"dst":    reqAddr,
-					}).Debug("HTTP request")
-				},
-				authFunc)
-			if err != nil {
-				logrus.WithField("error", err).Fatal("Failed to initialize HTTP server")
-			}
-			if config.HTTP.Cert != "" && config.HTTP.Key != "" {
-				logrus.WithField("addr", config.HTTP.Listen).Info("HTTPS server up and running")
-				errChan <- http.ListenAndServeTLS(config.HTTP.Listen, config.HTTP.Cert, config.HTTP.Key, proxy)
-			} else {
-				logrus.WithField("addr", config.HTTP.Listen).Info("HTTP server up and running")
-				errChan <- http.ListenAndServe(config.HTTP.Listen, proxy)
-			}
-		}()
-	}
-
-	if len(config.TUN.Name) != 0 {
-		go func() {
-			timeout := time.Duration(config.TUN.Timeout) * time.Second
-			if timeout == 0 {
-				timeout = 300 * time.Second
-			}
-			tunServer, err := tun.NewServer(client, transport.DefaultTransport,
-				time.Duration(config.TUN.Timeout)*time.Second,
-				config.TUN.Name, config.TUN.Address, config.TUN.Gateway, config.TUN.Mask, config.TUN.DNS, config.TUN.Persist)
-			if err != nil {
-				logrus.WithField("error", err).Fatal("Failed to initialize TUN server")
-			}
-			tunServer.RequestFunc = func(addr net.Addr, reqAddr string) {
-				logrus.WithFields(logrus.Fields{
-					"src": addr.String(),
-					"dst": reqAddr,
-				}).Debugf("TUN %s request", strings.ToUpper(addr.Network()))
-			}
-			tunServer.ErrorFunc = func(addr net.Addr, reqAddr string, err error) {
-				if err != nil {
-					if err == io.EOF {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-							"dst": reqAddr,
-						}).Debugf("TUN %s EOF", strings.ToUpper(addr.Network()))
-					} else if err == core.ErrClosed && strings.HasPrefix(addr.Network(), "udp") {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-							"dst": reqAddr,
-						}).Debugf("TUN %s closed for timeout", strings.ToUpper(addr.Network()))
-					} else if err.Error() == "deadline exceeded" && strings.HasPrefix(addr.Network(), "tcp") {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-							"dst": reqAddr,
-						}).Debugf("TUN %s closed for timeout", strings.ToUpper(addr.Network()))
-					} else {
-						logrus.WithFields(logrus.Fields{
-							"error": err,
-							"src":   addr.String(),
-							"dst":   reqAddr,
-						}).Infof("TUN %s error", strings.ToUpper(addr.Network()))
-					}
-				}
-			}
-			errChan <- tunServer.ListenAndServe()
-		}()
-	}
-
-	if len(config.TCPRelay.Listen) > 0 {
-		config.TCPRelays = append(config.TCPRelays, Relay{
-			Listen:  config.TCPRelay.Listen,
-			Remote:  config.TCPRelay.Remote,
-			Timeout: config.TCPRelay.Timeout,
-		})
-	}
-
-	if len(config.TCPRelays) > 0 {
-		for _, tcpr := range config.TCPRelays {
-			go func(tcpr Relay) {
-				rl, err := relay.NewTCPRelay(client, transport.DefaultTransport,
-					tcpr.Listen, tcpr.Remote,
-					time.Duration(tcpr.Timeout)*time.Second,
-					func(addr net.Addr) {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-						}).Debug("TCP relay request")
-					},
-					func(addr net.Addr, err error) {
-						if err != io.EOF {
-							logrus.WithFields(logrus.Fields{
-								"error": err,
-								"src":   addr.String(),
-							}).Info("TCP relay error")
-						} else {
-							logrus.WithFields(logrus.Fields{
-								"src": addr.String(),
-							}).Debug("TCP relay EOF")
-						}
-					})
-				if err != nil {
-					logrus.WithField("error", err).Fatal("Failed to initialize TCP relay")
-				}
-				logrus.WithField("addr", tcpr.Listen).Info("TCP relay up and running")
-				errChan <- rl.ListenAndServe()
-			}(tcpr)
-		}
-	}
-
-	if len(config.UDPRelay.Listen) > 0 {
-		config.UDPRelays = append(config.UDPRelays, Relay{
-			Listen:  config.UDPRelay.Listen,
-			Remote:  config.UDPRelay.Remote,
-			Timeout: config.UDPRelay.Timeout,
-		})
-	}
-
-	if len(config.UDPRelays) > 0 {
-		for _, udpr := range config.UDPRelays {
-			go func(udpr Relay) {
-				rl, err := relay.NewUDPRelay(client, transport.DefaultTransport,
-					udpr.Listen, udpr.Remote,
-					time.Duration(udpr.Timeout)*time.Second,
-					func(addr net.Addr) {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-						}).Debug("UDP relay request")
-					},
-					func(addr net.Addr, err error) {
-						if err != relay.ErrTimeout {
-							logrus.WithFields(logrus.Fields{
-								"error": err,
-								"src":   addr.String(),
-							}).Info("UDP relay error")
-						} else {
-							logrus.WithFields(logrus.Fields{
-								"src": addr.String(),
-							}).Debug("UDP relay session closed")
-						}
-					})
-				if err != nil {
-					logrus.WithField("error", err).Fatal("Failed to initialize UDP relay")
-				}
-				logrus.WithField("addr", udpr.Listen).Info("UDP relay up and running")
-				errChan <- rl.ListenAndServe()
-			}(udpr)
-		}
-	}
-
-	if len(config.TCPTProxy.Listen) > 0 {
-		go func() {
-			rl, err := tproxy.NewTCPTProxy(client, transport.DefaultTransport,
-				config.TCPTProxy.Listen, time.Duration(config.TCPTProxy.Timeout)*time.Second,
-				func(addr, reqAddr net.Addr) {
-					logrus.WithFields(logrus.Fields{
-						"src": addr.String(),
-						"dst": reqAddr.String(),
-					}).Debug("TCP TProxy request")
-				},
-				func(addr, reqAddr net.Addr, err error) {
-					if err != io.EOF {
-						logrus.WithFields(logrus.Fields{
-							"error": err,
-							"src":   addr.String(),
-							"dst":   reqAddr.String(),
-						}).Info("TCP TProxy error")
-					} else {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-							"dst": reqAddr.String(),
-						}).Debug("TCP TProxy EOF")
-					}
-				})
-			if err != nil {
-				logrus.WithField("error", err).Fatal("Failed to initialize TCP TProxy")
-			}
-			logrus.WithField("addr", config.TCPTProxy.Listen).Info("TCP TProxy up and running")
-			errChan <- rl.ListenAndServe()
-		}()
-	}
-
-	if len(config.UDPTProxy.Listen) > 0 {
-		go func() {
-			rl, err := tproxy.NewUDPTProxy(client, transport.DefaultTransport,
-				config.UDPTProxy.Listen, time.Duration(config.UDPTProxy.Timeout)*time.Second,
-				func(addr net.Addr) {
-					logrus.WithFields(logrus.Fields{
-						"src": addr.String(),
-					}).Debug("UDP TProxy request")
-				},
-				func(addr net.Addr, err error) {
-					if err != tproxy.ErrTimeout {
-						logrus.WithFields(logrus.Fields{
-							"error": err,
-							"src":   addr.String(),
-						}).Info("UDP TProxy error")
-					} else {
-						logrus.WithFields(logrus.Fields{
-							"src": addr.String(),
-						}).Debug("UDP TProxy session closed")
-					}
-				})
-			if err != nil {
-				logrus.WithField("error", err).Fatal("Failed to initialize UDP TProxy")
-			}
-			logrus.WithField("addr", config.UDPTProxy.Listen).Info("UDP TProxy up and running")
-			errChan <- rl.ListenAndServe()
 		}()
 	}
 
--- cmd/config.go
+++ cmd/config.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/sirupsen/logrus"
 	"github.com/yosuke-furukawa/json5/encoding/json5"
 )
 
@@ -104,36 +103,6 @@ type clientConfig struct {
 		User       string `json:"user"`
 		Password   string `json:"password"`
 	} `json:"socks5"`
-	HTTP struct {
-		Listen   string `json:"listen"`
-		Timeout  int    `json:"timeout"`
-		User     string `json:"user"`
-		Password string `json:"password"`
-		Cert     string `json:"cert"`
-		Key      string `json:"key"`
-	} `json:"http"`
-	TUN struct {
-		Name    string   `json:"name"`
-		Timeout int      `json:"timeout"`
-		Address string   `json:"address"`
-		Gateway string   `json:"gateway"`
-		Mask    string   `json:"mask"`
-		DNS     []string `json:"dns"`
-		Persist bool     `json:"persist"`
-	} `json:"tun"`
-	TCPRelays []Relay `json:"relay_tcps"`
-	TCPRelay  Relay   `json:"relay_tcp"` // deprecated, but we still support it for backward compatibility
-	UDPRelays []Relay `json:"relay_udps"`
-	UDPRelay  Relay   `json:"relay_udp"` // deprecated, but we still support it for backward compatibility
-	TCPTProxy struct {
-		Listen  string `json:"listen"`
-		Timeout int    `json:"timeout"`
-	} `json:"tproxy_tcp"`
-	UDPTProxy struct {
-		Listen  string `json:"listen"`
-		Timeout int    `json:"timeout"`
-	} `json:"tproxy_udp"`
-	ACL                 string `json:"acl"`
 	Obfs                string `json:"obfs"`
 	Auth                []byte `json:"auth"`
 	AuthString          string `json:"auth_str"`
@@ -147,48 +116,11 @@ type clientConfig struct {
 }
 
 func (c *clientConfig) Check() error {
-	if len(c.SOCKS5.Listen) == 0 && len(c.HTTP.Listen) == 0 && len(c.TUN.Name) == 0 &&
-		len(c.TCPRelay.Listen) == 0 && len(c.UDPRelay.Listen) == 0 &&
-		len(c.TCPRelays) == 0 && len(c.UDPRelays) == 0 &&
-		len(c.TCPTProxy.Listen) == 0 && len(c.UDPTProxy.Listen) == 0 {
+	if len(c.SOCKS5.Listen) == 0 {
 		return errors.New("please enable at least one mode")
 	}
 	if c.SOCKS5.Timeout != 0 && c.SOCKS5.Timeout <= 4 {
 		return errors.New("invalid SOCKS5 timeout")
-	}
-	if c.HTTP.Timeout != 0 && c.HTTP.Timeout <= 4 {
-		return errors.New("invalid HTTP timeout")
-	}
-	if c.TUN.Timeout != 0 && c.TUN.Timeout < 4 {
-		return errors.New("invalid TUN timeout")
-	}
-	if len(c.TCPRelay.Listen) > 0 && len(c.TCPRelay.Remote) == 0 {
-		return errors.New("no TCP relay remote address")
-	}
-	if len(c.UDPRelay.Listen) > 0 && len(c.UDPRelay.Remote) == 0 {
-		return errors.New("no UDP relay remote address")
-	}
-	if c.TCPRelay.Timeout != 0 && c.TCPRelay.Timeout <= 4 {
-		return errors.New("invalid TCP relay timeout")
-	}
-	if c.UDPRelay.Timeout != 0 && c.UDPRelay.Timeout <= 4 {
-		return errors.New("invalid UDP relay timeout")
-	}
-	for _, r := range c.TCPRelays {
-		if err := r.Check(); err != nil {
-			return err
-		}
-	}
-	for _, r := range c.UDPRelays {
-		if err := r.Check(); err != nil {
-			return err
-		}
-	}
-	if c.TCPTProxy.Timeout != 0 && c.TCPTProxy.Timeout <= 4 {
-		return errors.New("invalid TCP TProxy timeout")
-	}
-	if c.UDPTProxy.Timeout != 0 && c.UDPTProxy.Timeout <= 4 {
-		return errors.New("invalid UDP TProxy timeout")
 	}
 	if len(c.Server) == 0 {
 		return errors.New("no server address")
@@ -199,12 +131,6 @@ func (c *clientConfig) Check() error {
 	if (c.ReceiveWindowConn != 0 && c.ReceiveWindowConn < 65536) ||
 		(c.ReceiveWindow != 0 && c.ReceiveWindow < 65536) {
 		return errors.New("invalid receive window size")
-	}
-	if len(c.TCPRelay.Listen) > 0 {
-		logrus.Warn("'relay_tcp' is deprecated, please use 'relay_tcps' instead")
-	}
-	if len(c.UDPRelay.Listen) > 0 {
-		logrus.Warn("config 'relay_udp' is deprecated, please use 'relay_udps' instead")
 	}
 	return nil
 }
